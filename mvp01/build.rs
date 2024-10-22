042bd26b41cd742abaa634e9d99855a2697c8788
amuldotexe <amul.exe@gmail.com>
2024-10-22 07:59:28 +0530

    placeholder message 2024-10-22 07:59:28

--- mvp01/build.rs
+++
@@ -1,9 +0,0 @@
-use std::io::Result;
-use std::path::PathBuf;
-
-fn main() -> Result<()> {
-    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    tonic_build::compile_protos("proto/summary.proto")?;
-    println!("cargo:rerun-if-changed=proto/summary.proto");
-    Ok(())
-}
